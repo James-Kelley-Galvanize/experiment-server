31c4c7acfb5eabaeeafe8b004ddcedf3dc0f7c8f
James-Kelley-Galvanize <james.kelley@galvanize.com>
2023-01-17 09:22:31 -0700

    add logging of node environment to verify in logs

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -4,6 +4,8 @@ const allKnexConfigs = require("../knexfile");
 
 const knexConfig = allKnexConfigs[process.env.NODE_ENV || "development"];
 
+console.log(`NODE ENV is:`, process.env.NODE_ENV);
+
 const dbConnection = knex(knexConfig);
 
 module.exports = dbConnection;
